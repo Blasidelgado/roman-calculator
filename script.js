d6776eb1d83da090e5d7abe04cc2b310cd1527c4
Blasidelgado <blasidelgado@gmail.com>
2024-03-06 19:10:20 -0300

    Output shows error message if number lesser than 1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => console.log('Scripts loaded'));
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+    const userInput = document.getElementById('number')
+    const output = document.getElementById('output');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        if (userInput.value < 1) { // Return error message
+            output.innerText = 'Please enter a number greater than or equal to 1.';
+            output.classList.add('error');
+            return;
+        }
+    })
+});
