48487d4fc359cc93c705c5463746b88324fd27e3
Blasidelgado <blasidelgado@gmail.com>
2024-03-06 19:46:34 -0300

    Add upper bound limit to 3999 for conversion

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', e => {
         e.preventDefault();
-        if (userInput.value < 1) { // Return error message
-            output.innerText = 'Please enter a number greater than or equal to 1.';
+        const MIN = {
+            value: 1,
+            text: 'greater',
+        };
+        const MAX = {
+            value: 3999,
+            text: 'lesser',
+        };
+
+        const userInputValue = parseInt(userInput.value);
+        if (userInputValue < MIN.value || userInputValue > MAX.value) {
+            output.innerText = userInputValue < MIN.value ? 
+            `Please enter a number ${MIN.text} than or equal to ${MIN.value}.` :
+            `Please enter a number ${MAX.text} than or equal to ${MAX.value}.`;
             output.classList.add('error');
             return;
         }
